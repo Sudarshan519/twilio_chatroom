543240dac4d121e4e3227abc4bbb667d58bc01ed
Sudarshan519 <Sudarshanshrestha519@gmail.com>
2022-04-06 16:59:21 +0545

    twilio checking out

---
+++ lib/shared/twilio_service.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class TwilioFunctionsService {
+  TwilioFunctionsService._();
+  static final instance = TwilioFunctionsService._();
+
+  final http.Client client = http.Client();
+  final accessTokenUrl =
+      'https://twiliochatroomaccesstoken-3265.twil.io/accessToken';
+
+  Future<dynamic> createToken(String identity) async {
+    print("loading token");
+    try {
+      Map<String, String> header = {
+        'Content-Type': 'application/json',
+      };
+      var url = Uri.parse(accessTokenUrl + '?user=' + identity);
+      final response = await client.get(url, headers: header);
+      print(response.body);
+      Map<String, dynamic> responseMap = jsonDecode(response.body);
+      return responseMap;
+    } catch (error) {
+      throw Exception([error.toString()]);
+    }
+  }
+}
